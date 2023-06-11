d68327457a792c315c57a61f520036dbd5dfa3ab
coding4rever <propertyvaluecss@gmail.com>
2023-06-11 04:52:12 +0100

    Add Second Route

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8050; // default port 8080
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -22,4 +22,14 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
+  });
+
+app.get("/urls", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+  });
+
+  app.get("/urls/:id", (req, res) => {
+    const templateVars = { id: req.params.id, longURL: href="#" };
+    res.render("urls_show", templateVars);
   });
