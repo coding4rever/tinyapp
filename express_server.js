ae1719d8d6827645b894615829a253e35f44dfb5
coding4rever <propertyvaluecss@gmail.com>
2023-06-15 17:06:21 +0100

    get and post route register new user

--- express_server.js
+++ express_server.js
@@ -128,6 +128,21 @@ app.get("/urls", (req, res) => {
     res.redirect("/urls");
   });
   
+  app.get("/register", (req, res) => {
+    const templateVars = {
+      username: req.cookies['username']
+    };
+    res.render("user_registration", templateVars);
+  });
+
+  app.post("/register", (req, res) => {
+
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
+  });
+
   // app.post("/login", (req, res) => {
   //   const username = req.body.username;
   //   res.cookie("username", username);
