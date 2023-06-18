c4be809a0008364e5781f063e21ad5589bb47815
coding4rever <propertyvaluecss@gmail.com>
2023-06-18 05:04:05 +0100

    implemented tests

--- express_server.js
+++ express_server.js
@@ -3,28 +3,28 @@ const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
-//npx kill-port --port 8080 
+//npx kill-port --port 8080
 // npm install nodemon
 //npm run dev
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 
 app.use(express.urlencoded({ extended: true }));
 
 // declare all characters
-const characters ='hdfxfdxhjihidfghioehjtgjreio';
+const characters = "hdfxfdxhjihidfghioehjtgjreio";
 
 function generateString(length) {
-    let result = '';
-    const charactersLength = characters.length;
-    for ( let i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
+  let result = "";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
 
-    return result;
+  return result;
 }
 
 app.get("/", (req, res) => {
@@ -36,79 +36,76 @@ app.listen(PORT, () => {
 });
 
 app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-  });
+  res.json(urlDatabase);
+});
 
 app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
-  });
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
 
 app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase };
-    res.render("urls_index", templateVars);
-  });
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
-  app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
-  });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
-  /*app.get("/urls/:id", (req, res) => {
+/*app.get("/urls/:id", (req, res) => {
     const templateVars = { id: req.params.id, longURL: href="http://www.lighthouselabs.ca" };
     res.render("urls_show", templateVars);
   });*/
 
-  app.post("/urls", (req, res) => {
-    //console.log(req.body); // Log the POST request body to the console
-    //res.send("Ok"); // Respond with 'Ok' (we will replace this)
-    const longUrl = req.body.longURL
-    const shortUrl = generateString(6)
-    //Add to the database
-    urlDatabase[shortUrl] = longUrl
-    console.log(shortUrl)
-    console.log(urlDatabase)
-    //res.redirect(`/urls/${shortUrl}`);
-    res.redirect("/urls");
+app.post("/urls", (req, res) => {
+  //console.log(req.body); // Log the POST request body to the console
+  //res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const longUrl = req.body.longURL;
+  const shortUrl = generateString(6);
+  //Add to the database
+  urlDatabase[shortUrl] = longUrl;
+  console.log(shortUrl);
+  console.log(urlDatabase);
+  //res.redirect(`/urls/${shortUrl}`);
+  res.redirect("/urls");
+});
 
-  });
-
-  /*app.get("/urls/:id", (req, res) => {
+/*app.get("/urls/:id", (req, res) => {
     const shortUrl = req.params.shortUrl
     const templateVars = { id: shortUrl, longURL: urlDatabase[shortUrl] };
     res.render("urls_show", templateVars);
   });*/
 
-   app.get("/urls/:shortUrl", (req, res) => {
-    const shortUrl = req.params.shortUrl
-    const templateVars = { id: shortUrl, longURL: urlDatabase[shortUrl] };
-    res.render("urls_show", templateVars);
-  });
+app.get("/urls/:shortUrl", (req, res) => {
+  const shortUrl = req.params.shortUrl;
+  const templateVars = { id: shortUrl, longURL: urlDatabase[shortUrl] };
+  res.render("urls_show", templateVars);
+});
 
-  app.get("/u/:shortUrl", (req, res) => {
+app.get("/u/:shortUrl", (req, res) => {
   const longURL = urlDatabase[req.params.shortUrl];
   //console.log(longURL);
-  if(longURL){
-    res.redirect(longURL)  
-  }else{
-    res.status(404).send('URL Unavailable')
+  if (longURL) {
+    res.redirect(longURL);
+  } else {
+    res.status(404).send("URL Unavailable");
   }
-  });
+});
 
-  app.post("/urls/:shortUrl/delete", (req, res) => {
-    const shortUrl = req.params.shortUrl;
-    delete urlDatabase[shortUrl];
-    res.redirect('/urls');
-    console.log(shortUrl);
-   
-    console.log(urlDatabase);
-    console.log(urlDatabase[shortUrl]);
-    console.log(urlDatabase[shortUrl]);
-  })
+app.post("/urls/:shortUrl/delete", (req, res) => {
+  const shortUrl = req.params.shortUrl;
+  delete urlDatabase[shortUrl];
+  res.redirect("/urls");
+  console.log(shortUrl);
 
-  app.post("/urls/:shortUrl/edit", (req, res) => {
-    const shortUrl = req.params.shortUrl;
-    const newURL = req.body.updatedLongURL;
-    urlDatabase[shortUrl] = newURL;
-    res.redirect("/urls"); // Redirect to the index page after editing the URL
-  });
-  
-  
+  console.log(urlDatabase);
+  console.log(urlDatabase[shortUrl]);
+  console.log(urlDatabase[shortUrl]);
+});
+
+app.post("/urls/:shortUrl/edit", (req, res) => {
+  const shortUrl = req.params.shortUrl;
+  const newURL = req.body.updatedLongURL;
+  urlDatabase[shortUrl] = newURL;
+  res.redirect("/urls"); // Redirect to the index page after editing the URL
+});
